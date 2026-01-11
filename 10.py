1de55c12e74a518ec05410b89f8a0c785d40a36b
Raj <raj20063251@gmail.com>
2026-01-11 11:47:54 +0530

    updates

---
+++ 10.py
@@ -0,0 +1,5 @@
+while True:
+    number=int(input('Give a random number: '))
+    if number==0:
+        break
+print('done')
---
+++ 8.py
@@ -0,0 +1,7 @@
+age=int(input('What is your age kid :'))
+if age<13:
+    print('You r a child')
+elif age>13 and age<19:
+    print('you r a teen')
+else:
+    print('u r a adult')
